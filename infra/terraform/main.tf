af8c13637f74dff364e403b4bb5ff0f4d5358f31
Dan Herete <danheret@gmail.com>
2025-12-17 09:24:37 -0500

    Format Terraform files

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,25 +1,25 @@
-  terraform {
-    required_version = ">= 1.0"
-   
-    required_providers {
-      
-      aws = {
+terraform {
+  required_version = ">= 1.0"
 
-        source  = "hashicorp/aws"
-        
-        version = "~> 5.0"
-  
-       }
-   
-     }
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+
+      version = "~> 5.0"
+
+    }
 
   }
 
+}
 
-  # Configure AWS provider (credentials will be set later)
 
-  provider "aws" {
- 
-    region = "us-east-1"
+# Configure AWS provider (credentials will be set later)
 
-   } 
+provider "aws" {
+
+  region = "us-east-1"
+
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,12 @@
+
+
+variable "project_name" {
+
+  description = "Name of the DevOps project"
+
+  type = string
+
+  default = "devops-mastery"
+
+
+}
